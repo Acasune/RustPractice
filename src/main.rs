1373e777b885c1dbd60bf7307241d37c91c6e7d9
Acasune <tttt4aaaa@gmail.com>
2021-01-02 15:49:17 +0900

    p45 4th block

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+fn func(code: i32) -> Result<i32, String> {
+    println!("code: {}", code);
+    Ok(100)
+}
+
 fn main() {
     let result: Result<i32, String> = Ok(200);
-    println!("code: {}", result.unwrap_or(-1));
+    let next_result = result.and_then(func);
     let result: Result<i32, String> = Err("error".to_string());
-    println!("code: {}", result.unwrap_or(-1));
+    let next_result = result.and_then(func);
 }
