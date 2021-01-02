ef5829add19678452b57e300d3dc8d671f74eab8
Acasune <tttt4aaaa@gmail.com>
2021-01-02 16:06:08 +0900

    p49 the 1st block

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use std::vec;
-
 fn main() {
-  let v = vec![1,2,3,4,5];
-  println!("{}", v[0]);
+    let byte_array = [b'h', b'e', b'l', b'l', b'o'];
+    print(Box::new(byte_array));
+    let byte_array = [b'w',b'o', b'r', b'l', b'd', b'!'];
+    print(Box::new(byte_array));
+}
+
+fn print(s: Box<[u8]>) {
+    println!("{:?}", s);
 }
