dc54d7c12f7f5b7004c33f92c75a0e62be3213b3
Acasune <tttt4aaaa@gmail.com>
2021-01-02 15:51:41 +0900

    p47 the 1st block

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
-fn func(code: i32) -> Result<i32, String> {
-    println!("code: {}", code);
-    Ok(100)
-}
+use std::vec;
 
 fn main() {
-    let result: Result<i32, String> = Ok(200);
-    let next_result = result.and_then(func);
-    let result: Result<i32, String> = Err("error".to_string());
-    let next_result = result.and_then(func);
+  let v = vec![1,2,3,4,5];
+  println!("{}", v[0]);
 }
