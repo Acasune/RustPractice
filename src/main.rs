e794a44aa844a91a442ce00a43ea6915326f0083
Acasune <tttt4aaaa@gmail.com>
2021-01-02 17:01:40 +0900

    p57 the 1st block

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,32 @@
 fn main() {
-  let mut cnt = 0;
+    let it = Iter {
+        current: 0,
+        max: 10,
+    };
+    for num in it {
+        println!("{}", num);
+    }
 
-  let result = loop{
-      println!("count: {}", cnt);
-      cnt += 1;
-      if cnt == 10{
-          break cnt;
-      }
-  };
+}
+
+struct Iter {
+    current: usize,
+    max: usize,
+}
+
+impl Iterator for Iter {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.current += 1;
+        if self.current -1 < self.max {
+            Some(self.current -1)
+        } else {
+            None
+        }
+    }
+
+    // fn next($mut self) -> Option<usize> {
+
+    // }
 }
