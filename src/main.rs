88e689d0c4192a1c7cba8da9b9bb170d87e9c82f
Acasune <tttt4aaaa@gmail.com>
2021-01-02 15:43:24 +0900

    p45 3rd block

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 fn main() {
     let result: Result<i32, String> = Ok(200);
-
-    if let Ok(code) = result {
-        println!("code: {}", code)
-    }
+    println!("code: {}", result.unwrap_or(-1));
+    let result: Result<i32, String> = Err("error".to_string());
+    println!("code: {}", result.unwrap_or(-1));
 }
